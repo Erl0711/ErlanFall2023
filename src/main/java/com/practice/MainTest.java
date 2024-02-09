bf477793f08a2d43151023a1da3b85e23fa19d7c
Erlan <erlanc312@gmail.com>
2024-02-09 21:58:26 +0600

    add Car class

--- src/main/java/com/practice/MainTest.java
+++ src/main/java/com/practice/MainTest.java
@@ -1,7 +1,19 @@
 package com.practice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainTest {
     public static void main(String[] args) {
 
+        Car tesla = new Car("tesla", 3.5);
+        Car mers = new Car("tesla", 3.5);
+
+        Map<Car, String> map = new HashMap<>();
+
+        map.put(tesla, "modelS");
+
+        System.out.println(map.get(mers));
+        System.out.println(tesla.equals(mers));
     }
 }
