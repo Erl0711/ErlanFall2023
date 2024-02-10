b1b5c5360593409a7c66bf3a1413968521f99efc
Zhazgul <zhli230490@gmail.com>
2024-02-10 21:31:29 +0600

    Zhazgul_practice_package_commit

---
+++ src/main/java/com/practice/zhazgul_practice/_6_NaturalNums.java
@@ -0,0 +1,31 @@
+package com.practice.zhazgul_practice;
+
+import java.util.Scanner;
+
+public class _6_NaturalNums {
+    //    Выведите на экран все положительные делители натурального числа,
+//    введённого пользователем с клавиатуры.
+    public static void main(String[] args) {
+        naturalDigits();
+
+
+    }
+
+    public static void naturalDigits() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите натуральное число:\t");
+        int num = input.nextInt();
+        if (num <= 0) {
+            System.out.println("Invalid number, please try again");
+        } else {
+            System.out.println("Делителями числа "+num+" являются:\t");
+            for (int i =num; i >0; i--){
+                int b = num % i;
+                if (b == 0){
+                    System.out.println(i+ "");
+                }
+            }
+        }
+
+    }
+}
